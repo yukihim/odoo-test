796eb6f407d14e843889ccb264921ca4cc15a7ee
Yuyu <jacklorien@gmail.com>
2024-08-11 15:06:20 +0700

    first commit

---
+++ addons/real_estate_ads/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    "name": "Real Estate Ads",
+    "version": "1.0",
+    "website": "https://www.quanjf66.com",
+    "author": "Quan",
+    "description": """
+        Real Estate module to show available properties
+    """,
+    "category": "Sales",
+    "depends": [],
+    "data": [
+        'security/ir.model.access.csv'
+    ],
+    "installable": True,
+    "application": True,
+    "license": "LGPL-3"
+}
---
+++ addons/real_estate_ads/models/property.py
@@ -0,0 +1,31 @@
+from odoo import fields, models, api
+
+
+
+class Property(models.Model):
+    _name = 'estate.property'
+    _description = 'Estate Properties'
+    
+    name = fields.Char(string="Name", required=True)
+    description = fields.Char(string="Description")
+    postcode = fields.Char(string="Postcode")
+    date_availability = fields.Date(string="Available from")
+    expected_price = fields.Float(string="Expected Price")
+    best_offer = fields.Float(string="Best offer")
+    selling_price = fields.Float(string="Selling Price")
+    bedrooms = fields.Integer(string="Bedrooms")
+    living_area = fields.Integer(string="Living Area (sqm)")
+    facades = fields.Integer(string="Facades")
+    garage = fields.Boolean(string="Garage", default=False)
+    garden = fields.Boolean(string="Garden", default=False)
+    garden_area = fields.Integer(string="Garden Area (sqm)")
+    garden_orientation = fields.Selection(
+        [
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West')
+        ], string="Garden Orientation", default='north')
+    total_area = fields.Integer(string="Total Area (sqm)")
+    
+    id, create_date, create_uid, write_date, write_uid
